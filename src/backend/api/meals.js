013bf43ccbe08c0221cd5b2c54c1afeaf257650d
name <maha.karzoun@gmail.com>
2021-05-15 10:00:47 +0200

    adjust env config

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -5,11 +5,12 @@ const knex = require("../database");
 router.get("/", async (request, response) => {
   try {
     // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
+    const titles = await knex("meal").select("title");
     response.json(titles);
   } catch (error) {
     throw error;
   }
 });
+
 
 module.exports = router;
